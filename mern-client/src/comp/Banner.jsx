3dacfab3acfe50e6f616fa604eaa8246c3d624dc
rmrjSP <92806752+rmrjSP@users.noreply.github.com>
2024-04-23 14:26:33 -0700

    hehe

--- mern-client/src/comp/Banner.jsx
+++ mern-client/src/comp/Banner.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import BannerCard from '../home/BannerCard'
 
 const Banner = () => {
   return (
@@ -7,16 +8,16 @@ const Banner = () => {
         <div className='md:w-1/2 space-y-8 h-full'>
             <h2 className='text-5x1 font bold leading-snug text-black text-bold text-3xl'>Search across <span className='text-blue-700'>
                 media</span></h2>
-            <p className='md:w-4/5'>Find new titiles to consume across different media types based on your prefernces with multiple seach types</p>
+            <p className='md:w-4/5'>Find new titles to consume across different media types based on your prefernces with multiple seach types, there are over 70k titles across books, tv, anime etc.</p>
             <div>
                 <a href='https://reccomender-7ckb5mzvqoyqunerf8haxa.streamlit.app/'>
                     <button className='bg-blue-700 px-6 py-2 text-white font-medium hover:bg-black transition-all ease-in duration-200'>Search</button>
                 </a>
             </div>
         </div>
-
+        {/* banner cards */}
         <div>
-            right side
+          <BannerCard></BannerCard>
         </div>
       </div>
     </div>
