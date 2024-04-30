df49d895dcec30c6df94bdb0664153f5a1ad9dc6
rmrjSP <92806752+rmrjSP@users.noreply.github.com>
2024-04-30 15:15:11 -0700

    heheheheheheheheheheeh

---
+++ mern-client/src/AllAnimanga/AllAnimanga.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AllAnimanga = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AllAnimanga
--- mern-client/src/AllBooks/AllBooks.jsx
+++ mern-client/src/AllBooks/AllBooks.jsx
@@ -10,8 +10,12 @@ const AllBooks = () => {
   }, []);
   return (
     <div className='mt-28 px-4 lg;px-24'> 
+    <header className='display:flex '>
       <h2 className='text-5x1 font-bold text-center text-2xl'>All books</h2>
-
+      <a  href='https://reccomender-7ckb5mzvqoyqunerf8haxa.streamlit.app/'>
+                    <button className='bg-blue-700 px-6 py-2 text-white font-medium hover:bg-black transition-all ease-in duration-200'>Search + recommendations</button>
+                </a>
+    </header>
       <div className='grid gap-8 my-12 lg:grid-cols-4 sm:grid-cols-2 md:grid-cols-3 grid-cols-1'>
         {
           books.map(book => <Card
