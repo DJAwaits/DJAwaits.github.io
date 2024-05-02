ecc7512fbf7db4d49f2f5e76e6349756c41ca6ef
DJAwaits <padjcreeper@gmail.com>
2024-05-02 15:19:11 -0700

    5/2/2024
    a

--- mern-client/tailwind.config.js
+++ mern-client/tailwind.config.js
@@ -4,7 +4,15 @@ export default {
   "./src/**/*.{js,ts,jsx,tsx}",
   'node_modules/flowbite-react/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'celestial-blue': '#449dd1',
+        'sky-blue': '#78c0e0',
+        'Gunmetal': '#293040',
+        'Tan': 'c9ad8b',
+        'Gold': 'd5b263',
+      }
+    },
   },
   plugins: [
    require('flowbite/plugin')
