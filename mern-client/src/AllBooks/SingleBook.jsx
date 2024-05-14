a3416cb37530857c5b0e97075340e056ec131b93
DJAwaits <padjcreeper@gmail.com>
2024-05-14 15:17:10 -0700

    5/14/2024
    a

--- mern-client/src/AllBooks/SingleBook.jsx
+++ mern-client/src/AllBooks/SingleBook.jsx
@@ -9,11 +9,13 @@ const SingleBook = () => {
       <div className='m-2.5'>
         <h2 className='m-2.5 font-bold text-4xl border-b-2 border-gunmetal pb-1'>{title}</h2>
         <h2 className='m-2.5 mt-6 text-xl'>Description:</h2>
-        <p className='m-2.5 mt-2 border-2 rounded p-2 border-gunmetal'>{description}</p>
+        <p className='m-2.5 mt-2 border rounded p-2 border-gunmetal'>{description}</p>
         <h2 className='m-2.5 mt-6 text-xl'>Additional Information:</h2>
-        <p className='m-2.5 mt-2 border-2 rounded p-2 border-gunmetal'>{published_year} {num_pages}</p>
+        <p className='m-2.5 mt-2 border rounded p-2 border-gunmetal'>
+          <p>Published: {published_year}</p>
+          <p>Number of Pages: {num_pages}</p>
+        </p>
       </div>
-      
     </div>
   )
 }
